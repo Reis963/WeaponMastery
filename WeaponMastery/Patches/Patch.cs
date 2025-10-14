2327fc7311f72c10e112cdc1aef2288d4c5e264d
Diego <59540457+Reis963@users.noreply.github.com>
2025-10-14 11:31:08 -0300

    Initial project setup for Weapon Mastery plugin
    Add solution, project files, license, and README for the Weapon Mastery plugin. Implement basic plugin structure with BepInEx integration and a patch for weapon level randomization.

---
+++ WeaponMastery/Patches/Patch.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using SPT.Reflection.Patching;
+using Random = UnityEngine.Random;
+
+namespace WeaponMastery.Patches
+{
+    public class SetWeaponLevelPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(FirearmsAnimator).GetMethod("SetWeaponLevel", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(FirearmsAnimator __instance, float weaponLevel)
+        {
+            var randomWeaponLevel = Random.Range(0, 3);
+            WeaponAnimationSpeedControllerClass.SetWeaponLevel(__instance.Animator, randomWeaponLevel);
+        }
+    }
+}
---
+++ WeaponMastery/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using WeaponMastery.Patches;
+
+namespace WeaponMastery
+{
+    [BepInPlugin("com.reis963.weaponmastery", "Weapon Mastery", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal void Awake()
+        {
+            // Plugin startup logic
+            new SetWeaponLevelPatch().Enable();
+        }
+    }
+}
